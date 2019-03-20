b8a6731a4b9fd031d171de9c73b3dcf241cf2dec
Ian Guimarães <ianguimaraes31@gmail.com>
2019-03-20 05:18:58 -0300

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,4 +1,4 @@
 import './view/nav.js';
 import './view/film-info.js';
 import './view/text-input.js';
-import '../node_modules/@polymer/paper-input/paper-input';
+import '../node_modules/@polymer/paper-input/paper-input.js';
